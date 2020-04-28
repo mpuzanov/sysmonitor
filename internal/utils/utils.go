0027627b77714f593d42ad4e0ad751b86b21ad12
Mikhail Puzanov <mpuzanov@mail.ru>
2020-04-28 20:43:35 +0400

    добавил функцию округления float64

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,13 +1,20 @@
 package utils
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
 
-// RandFloats .
+// RandFloats случайное значение из диопазона
 func RandFloats(min, max float64) float64 {
 	rand.Seed(time.Now().UnixNano())
 	r := min + rand.Float64()*(max-min)
 	return r
+}
+
+// RandToFixed
+func RandToFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(math.Round(num*output)) / output
 }
