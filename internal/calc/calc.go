10951b54f821a1fc8fe4d94b7cde8bd805eee1a3
Mikhail Puzanov <mpuzanov@mail.ru>
2020-05-04 20:27:11 +0400

    переменовал пакет

--- internal/calc/calc.go
+++ internal/calc/calc.go
@@ -1,4 +1,4 @@
-package utils
+package calc
 
 import (
 	"math"
@@ -6,15 +6,15 @@ import (
 	"time"
 )
 
-// RandFloats случайное значение из диопазона
+// RandFloats случайное значение из диапазона
 func RandFloats(min, max float64) float64 {
 	rand.Seed(time.Now().UnixNano())
 	r := min + rand.Float64()*(max-min)
 	return r
 }
 
-// RandToFixed округление с заданным значением точности
-func RandToFixed(num float64, precision int) float64 {
+// RoundToFixed округление с заданным значением точности
+func RoundToFixed(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
 	return float64(math.Round(num*output)) / output
 }
