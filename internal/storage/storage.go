da3e4793ca0b41ef51cf6f056abd2c6ce30e06e5
Mikhail Puzanov <mpuzanov@mail.ru>
2020-05-17 18:14:32 +0400

    lint

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -7,11 +7,11 @@ import (
 
 // Storage интерфейс для работы с DB
 type Storage interface {
-	StorageLoadSystem
-	StorageLoadCPU
-	StorageLoadDisk
-	StorageTalkersNet
-	StorageNetworkStatistics
+	LoadSystem
+	LoadCPU
+	LoadDisk
+	TalkersNet
+	NetworkStatistics
 }
 
 // NewMemoryStorage create storage for sysmonitor
@@ -19,40 +19,40 @@ func NewMemoryStorage() Storage {
 	return memory.NewSystemStore()
 }
 
-// StorageLoadSystem интерфейс для работы с LoadSystem
-type StorageLoadSystem interface {
+// LoadSystem интерфейс для работы с LoadSystem
+type LoadSystem interface {
 	//SaveLoadSystem сохранение информации
 	SaveLoadSystem(data *model.LoadSystem) error
 	//GetAvgLoadSystem получение средней информации за период
 	GetAvgLoadSystem(period int32) (model.LoadSystem, error)
 }
 
-// StorageLoadCPU интерфейс для работы с CPU
-type StorageLoadCPU interface {
+// LoadCPU интерфейс для работы с CPU
+type LoadCPU interface {
 	//SaveLoadCPU сохранение информации по CPU
 	SaveLoadCPU(data *model.LoadCPU) error
 	//GetAvgLoadCPU получение средней информации за период
 	GetAvgLoadCPU(period int32) (model.LoadCPU, error)
 }
 
-// StorageLoadDisk интерфейс для работы с дисками
-type StorageLoadDisk interface {
+// LoadDisk интерфейс для работы с дисками
+type LoadDisk interface {
 	//SaveLoadDisk сохранение информации по дискам
 	SaveLoadDisk(data *model.LoadDisk)
 	//GetInfoDisk получение информации по дискам
 	GetInfoDisk() model.LoadDisk
 }
 
-// StorageTalkersNet интерфейс для работы с top talkers сети
-type StorageTalkersNet interface {
+// TalkersNet интерфейс для работы с top talkers сети
+type TalkersNet interface {
 	//SaveTalkersNet сохранение информации
 	SaveTalkersNet(data *model.TalkersNet) error
 	//GetAvgTalkersNet получение средней информации за период
 	GetAvgTalkersNet(period int32) (model.TalkersNet, error)
 }
 
-// StorageNetworkStatistics интерфейс для работы со статистикой по сетевым соединениям
-type StorageNetworkStatistics interface {
+// NetworkStatistics интерфейс для работы со статистикой по сетевым соединениям
+type NetworkStatistics interface {
 	//SaveTalkersNet сохранение информации
 	SaveNetworkStatistics(data *model.NetworkStatistics) error
 	//GetAvgTalkersNet получение средней информации за период
